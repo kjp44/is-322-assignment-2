3a68e4a8c4c342a3f68987405d6fa0116dd7c3b6
Kyle Payne <kjp44@njit.edu>
2019-04-03 19:10:38 -0400

    Added non-functioning form, updated README, made link from dashboard to add form and vice versa

--- client/src/pages/TaskForm.tsx
+++ client/src/pages/TaskForm.tsx
@@ -1,9 +1,51 @@
 import React, { Component } from "react";
+import { Task } from "../components/task";
+import { TaskContext } from "../contexts";
+import {Link} from "react-router-dom";
 
 class TaskForm extends Component {
   render() {
-    return <div>THIS IS TASK FORM</div>;
+    return (
+      <TaskContext.Consumer>
+        {value => {
+          return value ? (
+            <React.Fragment>
+              <Link className="m-5 text-info" to="/">Dashboard</Link>
+              <div className="row d-flex m-0 justify-content-center">
+                <div className="col-6 col-sm-6 col-md-4 col-lg-4 border h-100 m-1">
+                <h2 className="mt-2">Add New Task</h2>
+                <form>
+                	<label>
+                		<strong>Task Title</strong>
+                		<br />
+                		<input type="text" />
+                	</label>
+                	<br />
+                	<label>
+                		<strong>Task Type</strong>
+                		<br />
+                		<input type="text" />
+                	</label>
+                	<br />
+                	<label>
+                		<strong>Task ID</strong>
+                		<br />
+                		<input type="text" />
+                	</label>
+                	<br />
+                	<input className="btn btn-info m-2" type="submit" value="Submit" />
+                </form>
+                </div>
+              </div>
+            </React.Fragment>
+          ) : (
+            <div>Loading...</div>
+          );
+        }}
+      </TaskContext.Consumer>
+    );
   }
 }
+
 
 export default TaskForm;
